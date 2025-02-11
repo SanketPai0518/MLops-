fa7b248177624b446d00d55514ab4089410a7442
SanketPai0518 <111495475+SanketPai0518@users.noreply.github.com>
2025-02-11 13:50:14 +0530

    project structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    nmae='src',
+    version="0.0.1",
+    author="Sanket Pai",
+    author_email="sanketsushant18@gmail.com",
+    packages=find_packages()
+)
